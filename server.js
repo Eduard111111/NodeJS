8edaedb7584fb713b9cf5afa38960ffc0e69f267
Eduard111111 <132599050+Eduard111111@users.noreply.github.com>
2023-05-08 20:36:32 +0300

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-console.log("Hello!")
-console.log()
-function random (min, max) {
-    return Math.floor(Math.random()*(max-min))+min;
-//
-}
-console.log(process.argv)
-const randomNum = random(process.argv[2], process.argv[3])
-console.log(randomNum)
